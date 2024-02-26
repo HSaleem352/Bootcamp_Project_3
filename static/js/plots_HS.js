3c40cf274fec49af14dae35637be26280be6ed87
HSaleem352 <60048058+HSaleem352@users.noreply.github.com>
2024-02-25 22:13:17 -0500

    graphing

--- static/js/plots_HS.js
+++ static/js/plots_HS.js
@@ -1,5 +1,31 @@
 // Use D3 to fetch data from the API endpoint
 
 d3.json("/residence").then(function(response) {
-  console.log(response)
+  console.log(response.count)
+  dict = response.count
+  names = []
+  values = []
+
+  for (let key in dict) {
+    if (dict.hasOwnProperty(key)) {
+      names.push(key)
+      values.push(dict[key])
+      // console.log("Key:", key);
+      // console.log("Value:", dict[key]);
+    }
+  }
+  
+  let trace1 = {
+    x: names,
+    y: values,
+    type: "bar"
+  };
+
+  let layout = {
+    title: "residence_counts"
+
+  };
+
+  Plotly.newPlot("plot", trace1, layout);
+
   });
