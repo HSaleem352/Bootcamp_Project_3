2331a953b5693b86c4bce6576649919ccef39b03
Mina <Minalbm77@gmail.com>
2024-02-26 11:35:30 -0500

    updated

--- Project3/app.py
+++ Project3/app.py
@@ -4,22 +4,33 @@ import pandas as pd
 
 app = Flask(__name__)
 
-# Replace 'your_file.csv' with the actual path to your CSV file
-csv_file_path = 'Smoker&BC_VS_Non-Smoker&BC.csv'
-
-@app.route('/api/data', methods=['GET'])
-def get_data():
-    # Read CSV file using pandas
-    df = pd.read_csv(csv_file_path)
-    
-    # Convert DataFrame to JSON
-    json_data = df.to_json(orient='records')
-
-    return json_data
+# CSV files
+csv1 = 'Smoker&BC_VS_Non-Smoker&BC.csv'
+csv2 = 'Smoker&BC_VS_Non-Smoker&BC.csv'
 
 @app.route('/')
 def main():
     return render_template("index.html")
+
+@app.route('/api/Smoker_NonSmoker_Covid_DF', methods=['GET'])
+def get_data1():
+    # Read CSV file using pandas
+    df1 = pd.read_csv(csv1)
+    
+    # Convert DataFrame to JSON
+    json_data = df1.to_json(orient='records')
+
+    return json_data
+
+@app.route('/api/Smoker&BC_VS_Non-Smoker&BC', methods=['GET'])
+def get_data2():
+    # Read CSV file using pandas
+    df2 = pd.read_csv(csv2)
+    
+    # Convert DataFrame to JSON
+    json_data = df2.to_json(orient='records')
+
+    return json_data
 
 if __name__ == '__main__':
     app.run(debug=True)
