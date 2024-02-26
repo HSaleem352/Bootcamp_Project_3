a45d7704214a9f4714ea75fb3074544922420ffb
Mina <Minalbm77@gmail.com>
2024-02-26 15:34:55 -0500

    Update app.py

--- Project3/app.py
+++ Project3/app.py
@@ -6,10 +6,10 @@ app = Flask(__name__)
 
 # CSV files
 # csv1 = 'Smoker&BC_VS_Non-Smoker&BC.csv'
-csv2 = 'Smoker_NonSmoker_Covid_DF.csv'
-csv3 = 'MildCov_BC_Smoker_NonSmoker_DF.csv'
-csv4 = 'ModerateCov_BC_Smoker_NonSmoker_DF.csv'
-csv5 = 'SevereCov_BC_Smoker_NonSmoker_DF.csv'
+# csv2 = 'Smoker_NonSmoker_Covid_DF.csv'
+# csv3 = 'MildCov_BC_Smoker_NonSmoker_DF.csv'
+# csv4 = 'ModerateCov_BC_Smoker_NonSmoker_DF.csv'
+# csv5 = 'SevereCov_BC_Smoker_NonSmoker_DF.csv'
 
 @app.route('/')
 def main():
@@ -28,7 +28,7 @@ def get_data1():
 @app.route('/api/Smoker_NonSmoker_Covid_DF', methods=['GET'])
 def get_data2():
     # Read CSV file using pandas
-    df2 = pd.read_csv(csv2)
+    df2 = pd.read_csv('https://drive.google.com/uc?id=1uZaOgO0ly8vnpRrjx8L8D-D0bit453Hh&export=download')
     
     # Convert DataFrame to JSON
     json_data = df2.to_json(orient='records')
@@ -38,7 +38,7 @@ def get_data2():
 @app.route('/api/MildCov_BC_Smoker_NonSmoker_DF', methods=['GET'])
 def get_data3():
     # Read CSV file using pandas
-    df2 = pd.read_csv(csv3)
+    df2 = pd.read_csv('https://drive.google.com/uc?id=1eGS51t_iA2thfhZPX4dwlNeHRSldqSrL&export=download')
     
     # Convert DataFrame to JSON
     json_data = df2.to_json(orient='records')
@@ -48,7 +48,7 @@ def get_data3():
 @app.route('/api/ModerateCov_BC_Smoker_NonSmoker_DF', methods=['GET'])
 def get_data4():
     # Read CSV file using pandas
-    df2 = pd.read_csv(csv4)
+    df2 = pd.read_csv('https://drive.google.com/uc?id=1ByrRT6n_V9kNpAordMIipHB6k4pWC4xo&export=download')
     
     # Convert DataFrame to JSON
     json_data = df2.to_json(orient='records')
@@ -58,7 +58,7 @@ def get_data4():
 @app.route('/api/SevereCov_BC_Smoker_NonSmoker_DF', methods=['GET'])
 def get_data5():
     # Read CSV file using pandas
-    df2 = pd.read_csv(csv5)
+    df2 = pd.read_csv('https://drive.google.com/uc?id=1Cn75SkCm_0x3Z7VKsifnpeavcJZDalJL&export=download')
     
     # Convert DataFrame to JSON
     json_data = df2.to_json(orient='records')
