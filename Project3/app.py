77078c27793e7d4919679c38a973c197a58d9e95
Mina <Minalbm77@gmail.com>
2024-02-27 14:59:22 -0500

    Update app.py

--- Project3/app.py
+++ Project3/app.py
@@ -12,6 +12,10 @@ def main():
 def mina_q1():
     return render_template("mina_q1.html")
 
+@app.route('/mina_q2')
+def mina_q2():
+    return render_template("mina_q2.html")
+
 @app.route('/api/Smoker&BC_VS_Non-Smoker&BC', methods=['GET'])
 def get_data1():
     # Read CSV file using pandas
@@ -61,10 +65,6 @@ def get_data5():
     json_data = df.to_json(orient='records')
 
     return json_data
-
-@app.route('/mina_q2')
-def mina_q2():
-    return render_template("mina_q2.html")
 
 @app.route('/api/A_Res_BC_Smoker_NonSmoker_DF', methods=['GET'])
 def get_data6():
