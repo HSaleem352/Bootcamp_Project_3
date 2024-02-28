5202d22e8c904249628b61ef3f05e5dc23843375
HSaleem352 <60048058+HSaleem352@users.noreply.github.com>
2024-02-28 00:03:36 -0500

    update_flask for SQL

--- Project3/app.py
+++ Project3/app.py
@@ -68,25 +68,6 @@ def get_data5():
 
 @app.route('/HS/residence')
 def get_data():
-    # Reading the file
-    #dataset_df = pd.read_csv("https://drive.google.com/u/0/uc?id=1IBWDyHq2TJHcvYXWJ8Kh38j_L_ZlJbkr&export=download")
-
-    # Cleaning the dataset
-    # Narrow down the dataset to view region and severity of Covid
-    # residence_df = dataset_df[['der_region_v2', 'urban_rural', 'der_site_type', 'severity_of_covid_19_v2']]
-
-    # # Clean the dataset to remove any NAN fields
-    # nonNaN_residence_df = residence_df.dropna()
-
-    # # Remove any non-US and other data
-    # US_residence_df = nonNaN_residence_df.loc[(nonNaN_residence_df['der_region_v2'] != 'Non-US') | (nonNaN_residence_df['der_region_v2'] != 'Other')]
-
-    # # Remove unknown residence 
-    # US_residence_df_clean = US_residence_df.loc[(US_residence_df['urban_rural'] != 'Unknown') | (US_residence_df['der_region_v2'] != 'Other')]
-
-    # #residence_counts = pd.DataFrame(US_residence_df_clean['urban_rural'].value_counts())
-    # residence_counts = US_residence_df_clean['urban_rural'].value_counts()
-    # residence_counts = pd.DataFrame(US_residence_df_clean['urban_rural'].value_counts())
 
     with engine.connect() as connection:
         residence_counts = pd.read_sql('residence_counts',connection)
--- Project3/static/js/plots_HS.js
+++ Project3/static/js/plots_HS.js
@@ -1,5 +1,3 @@
-import plotly.graph_objects as go
-
 d3.json("/HS/residence").then(function(response) {
 
   var names = response.map(d => d.urban_rural);
@@ -8,55 +6,21 @@ d3.json("/HS/residence").then(function(response) {
   var trace = [{
     values: values,
     labels: names,
+    domain: {column: 0},
+    hole: .4,
     type: 'pie'
   }];
 
   var layout = {
+    title: "Distribution of Patients' residence",
+
       height: 600,
-      width: 600
+      width: 600,
+      
     };
 
-  fig = go.Figure(data=[go.Pie(labels=names, values=values, hole=.5)])
-
-  Plotly.newPlot("HS_pie", fig, layout);
+  Plotly.newPlot("HS_pie", trace, layout);
 
 
 });
 
-
-
-
-
-
-
-
-
-  // console.log(response)
-  // dict = response
-  // names = []
-  // values = []
-
-  // for (let key in dict) {
-  //   if (dict.hasOwnProperty(key)) {
-  //     names.push(key)
-  //     values.push(dict[key])
-      // console.log("Key:", key);
-      // console.log("Value:", dict[key]);
-  //   }
-  // }
-
-  // var trace1 = [{
-  //   values: values,
-  //   labels: names,
-  //   type: 'pie'
-  // }];
-  
-  // var layout = {
-  //   height: 400,
-  //   width: 500
-  // };
-
-
-  // Plotly.newPlot("HS_pie", trace1, layout);
-
-  
