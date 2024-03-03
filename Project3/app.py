3aa84f54d77ffa02a0d9ab687c4c03bc8484eec4
AlejandraFeatherston <alejandra.featherston@gmail.com>
2024-03-03 18:15:43 -0500

    progress

--- Project3/app.py
+++ Project3/app.py
@@ -15,7 +15,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def main():
-    return render_template("index.html")
+    return render_template("alejandra.html")
 
 #################################################################################################################
 ##                                                Mina                                                         ##
