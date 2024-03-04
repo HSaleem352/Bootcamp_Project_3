f45bd025effa6f200d897a694f49c8dc450f8ce3
HSaleem352 <60048058+HSaleem352@users.noreply.github.com>
2024-03-03 21:45:37 -0500

    changes

--- Project3/app.py
+++ Project3/app.py
@@ -2,6 +2,15 @@ from flask import Flask, render_template, jsonify
 import pandas as pd
 import os
 
+# Python SQL toolkit and Object Relational Mapper
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine
+import pandas as pd
+
+engine = create_engine('postgresql+psycopg2://breast_cancer_dataset_user:UnSNEeECgY7ky2i5KAPC2WtQn9XrRpvc@dpg-cnbvjf779t8c73epbb3g-a.oregon-postgres.render.com/breast_cancer_dataset')
+
 app = Flask(__name__)
 
 
@@ -15,27 +24,39 @@ def fozia_page():
 
 @app.route('/obesity_age_effect')
 def obesity_age_effect():
-    # Call the function to collect and clean the data if not already done
-    # cleaned_data_link = "https://drive.google.com/file/d/1XlddGFnRFavw58PFowIIgLVWrRco-Ffu/view?usp=drive_link"
+    with engine.connect() as connection:
+        mild = pd.read_sql("SELECT der_age_trunc FROM obesity_age_effect Where severity_of_covid_19_v2 = 'Mild'", connection)
+        moderate = pd.read_sql("SELECT der_age_trunc FROM obesity_age_effect Where severity_of_covid_19_v2 = 'Moderate'", connection)
+        severe = pd.read_sql("SELECT der_age_trunc FROM obesity_age_effect Where severity_of_covid_19_v2 = 'Severe'", connection)
+        
+    combine1 = mild.join(moderate, lsuffix='_caller', rsuffix='_other')
+    combine2 = combine1.join(severe, lsuffix='_caller', rsuffix='_other')
+    combine2.rename(columns={"der_age_trunc_caller": "mild", "der_age_trunc_other": "moderate", "der_age_trunc":"Severe"}, inplace = True)
 
-    # Reading the data from Google Drive
-    file_link = 'https://drive.google.com/u/0/uc?id=1IBWDyHq2TJHcvYXWJ8Kh38j_L_ZlJbkr&export=download'
-    data_df = pd.read_csv(file_link)
+    age_obesity = combine2.fillna(0)
     
-    # Cleaning and filtering the data 
-    data_df["der_days_fu"] = pd.to_numeric(data_df["der_days_fu"], errors="coerce")
-    data_df["der_days_fu"] = pd.to_numeric(data_df["der_age_trunc"], errors="coerce")
+    
+    # # Call the function to collect and clean the data if not already done
+    # # cleaned_data_link = "https://drive.google.com/file/d/1XlddGFnRFavw58PFowIIgLVWrRco-Ffu/view?usp=drive_link"
 
-    # Selecting only the columns needed
-    selected_columns = ["der_age_trunc", "der_obesity", "der_race_v2", "der_cancer_status_v4", "severity_of_covid_19_v2"]
-    cleaned_data = data_df[selected_columns]
+    # # Reading the data from Google Drive
+    # file_link = 'https://drive.google.com/u/0/uc?id=1IBWDyHq2TJHcvYXWJ8Kh38j_L_ZlJbkr&export=download'
+    # data_df = pd.read_csv(file_link)
+    
+    # # Cleaning and filtering the data 
+    # data_df["der_days_fu"] = pd.to_numeric(data_df["der_days_fu"], errors="coerce")
+    # data_df["der_days_fu"] = pd.to_numeric(data_df["der_age_trunc"], errors="coerce")
 
-    # Dropping rows with empty values 
-    cleaned_data = cleaned_data.dropna()
+    # # Selecting only the columns needed
+    # selected_columns = ["der_age_trunc", "der_obesity", "der_race_v2", "der_cancer_status_v4", "severity_of_covid_19_v2"]
+    # cleaned_data = data_df[selected_columns]
+
+    # # Dropping rows with empty values 
+    # cleaned_data = cleaned_data.dropna()
     
     # cleaned_data = collect_and_clean_data()
     # Convert the cleaned data to JSON
-    cleaned_data_json = cleaned_data.to_json(orient='records')
+    cleaned_data_json = age_obesity.to_json(orient='records')
 
     return cleaned_data_json
 
--- Project3/static/js/plots_FY.js
+++ Project3/static/js/plots_FY.js
@@ -3,13 +3,39 @@
 
 d3.json("/obesity_age_effect").then(function(data) {
 
+    var x = ["Mild", "Moderate", "Severe"];
+    var y1 = data.map(d => d.mild);
+    var y2 = data.map(d => d.moderate);
+    var y3 = data.map(d => d.severe);
+
     console.log(data)
 
-    // trace1 = createHistogram(data);
-    // trace2 = createBarChart(data);
-    // trace3 = createBoxplot(data);
+    
 
-    // Plotly.newPlot("histogram-container", trace1, trace2, trace3);
+
+    var trace1 = {
+        x: x,
+        y: y1,
+        type: 'box'
+      };
+      
+      var trace2 = {
+        x: x,
+        y: y2,
+        type: 'box'
+      };
+
+      var trace3 = {
+        x: x,
+        y: y3,
+        type: 'box'
+      };
+      
+      var data = [trace1, trace2, trace3];
+    
+    Plotly.newPlot('boxplot-container', data);
+
+    
 
 });
 
@@ -29,17 +55,17 @@ d3.json("/obesity_age_effect").then(function(data) {
 //         .catch(error => console.error('Error fetching data:', error));
 // }
 
-d3.json("/obesity_age_effect").then(function(data) {
+// d3.json("/obesity_age_effect").then(function(data) {
 
-    console.log(data)
+//     console.log(data)
 
-    // trace1 = createHistogram(data);
-    // trace2 = createBarChart(data);
-    // trace3 = createBoxplot(data);
+//     // trace1 = createHistogram(data);
+//     // trace2 = createBarChart(data);
+//     // trace3 = createBoxplot(data);
 
-    // Plotly.newPlot("histogram-container", trace1, trace2, trace3);
+//     // Plotly.newPlot("histogram-container", trace1, trace2, trace3);
 
-});
+// });
 
 
 // // Visualization function for COVID-19 severity distribution (Histogram)
