1445ff71bea1493748942c8850d835871598c1f9
HSaleem352 <60048058+HSaleem352@users.noreply.github.com>
2024-03-06 15:32:22 -0500

    api endpoint for shan

--- Project3/app.py
+++ Project3/app.py
@@ -231,6 +231,9 @@ def afr_page():
 ##                                                  Shan                                                       ##
 #################################################################################################################
 
+@app.route('/race_ethnicity_affect_COVID')
+def afr_page():
+    return render_template("shan.html")
 
 @app.route('/api/v1/percentage_df', methods=['GET'])
 def get_data1_shan():
